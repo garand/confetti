58a3e36e0045dc15a79ab74731774535d49b8723
Anthony Garand <garand@garand.me>
2015-07-22 21:36:56 -0400

    Update landing page

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Confetti</title>
+  <meta name="viewport" content="width=device-width,initial-scale=.75,user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="/assets/style/main.css">
+</head>
+<body>
+  <h1 class="logo">Confetti</h1>
+  <h3 class="description">Private group messaging</h3>
+  <a class="button" href="http://github.com/garand/confetti">GitHub</a>
+  <script type="text/javascript">
+    var _gauges = _gauges || [];
+    (function() {
+      var t   = document.createElement('script');
+      t.type  = 'text/javascript';
+      t.async = true;
+      t.id    = 'gauges-tracker';
+      t.setAttribute('data-site-id', '5514334bde2e261e74000eea');
+      t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+      t.src = 'https://track.gaug.es/track.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(t, s);
+    })();
+  </script>
+</body>
+</html>
