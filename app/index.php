68315e28b8fc902b78343af74ccd8a6cf9f46dfc
Anthony Garand <garand@garand.me>
2015-07-24 16:19:43 -0400

    Split app and splash page

--- app/index.php
+++
@@ -1,7 +0,0 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/template/header.php'; ?>
-
-<header>
-  <h1 class="logo">Confetti</h1>
-</header>
-
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/app/template/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,33 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Confetti</title>
-  <meta name="viewport" content="width=device-width,initial-scale=.75,user-scalable=no">
-  <link rel="stylesheet" type="text/css" href="/assets/style/main.css">
-  <link rel="apple-touch-icon" href="/assets/images/app_icon_180.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/assets/images/app_icon_120.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/app_icon_180.png">
-</head>
-<body>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/template/header.php'; ?>
+
+<body class="login">
   <h1 class="logo">Confetti</h1>
-  <h3 class="description">Private group messaging</h3>
-  <div class="buttons">
-    <a class="button" href="http://github.com/garand/confetti">GitHub</a>
-    <a class="button secondary" href="http://app.confetti.io">Login</a>
-  </div>
-  <script type="text/javascript">
-    var _gauges = _gauges || [];
-    (function() {
-      var t   = document.createElement('script');
-      t.type  = 'text/javascript';
-      t.async = true;
-      t.id    = 'gauges-tracker';
-      t.setAttribute('data-site-id', '5514334bde2e261e74000eea');
-      t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-      t.src = 'https://track.gaug.es/track.js';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(t, s);
-    })();
-  </script>
+  <form action="" method="post">
+    <label for="email">Email Address</label>
+    <input type="text" id="email" name="email">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+    <input type="submit" value="Login">
+    <a href="/login/forgot-password">Forgot Password?</a>
+  </form>
 </body>
-</html>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/template/footer.php'; ?>
